a62948ac381ac55f3c81a59adfd9329a57d7bfc5
Anjaliraghu123 <anjalifromkpm@gmail.com>
2026-01-18 11:48:28 +0530

    PR changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Header from"./Components/Header/Header.jsx"
+import './App.scss';
+import Header from"./Components/Header/Header"
 import Home from "./Components/Pages/Home/Home";
-import Contact from"./Components/Pages/Contact/Contact.jsx"
+import Contact from "./Components/Pages/Contact/Contact";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+  <div className="App">
     <Header/>
-
-     <Routes>
-        <Route path="/" element={<Home />} />
-         <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </>
-
-    
-  );
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </div>
+);
 }
 
 export default App;
