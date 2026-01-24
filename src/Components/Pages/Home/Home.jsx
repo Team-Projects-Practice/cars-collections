44da29a7deaca6f7c52c7df7b3aacd58a1b7ebc0
Anjaliraghu123 <anjalifromkpm@gmail.com>
2026-01-24 16:58:57 +0530

    changes Completed

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { CartypeProvider } from '../../../Homecontext/Homecontext';
 import Banner from '../../Banner/Banner';
 import Carbodystyles from '../../Carbodystyles/Carbodystyles';
-import Features from '../../Features/Features';
 const Home = () => {
   return (
     <div>
-   <Banner/>
-   <Carbodystyles/>
-   <Features/>
-   <CartypeProvider/>
-       </div>
+      <CartypeProvider>
+        <Banner />
+        <Carbodystyles />
+      </CartypeProvider>
+    </div>
   )
 }
 
