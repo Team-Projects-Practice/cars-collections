2b82751411f6c1a8f83fb65e647e80a112ca2cb6
Anjaliraghu123 <anjalifromkpm@gmail.com>
2026-01-24 16:37:41 +0530

    Homepage changes

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { CartypeProvider } from '../../../Homecontext/Homecontext';
 import Banner from '../../Banner/Banner';
+import Carbodystyles from '../../Carbodystyles/Carbodystyles';
 import Features from '../../Features/Features';
 const Home = () => {
   return (
     <div>
    <Banner/>
+   <Carbodystyles/>
    <Features/>
    <CartypeProvider/>
        </div>
