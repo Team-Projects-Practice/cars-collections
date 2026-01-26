b4712b15a33882451659d1c6e370ca208b2c0305
Anjaliraghu123 <anjalifromkpm@gmail.com>
2026-01-26 12:33:00 +0530

    code refactor

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,15 +3,31 @@ import { CartypeProvider } from '../../../Homecontext/Homecontext';
 import Banner from '../../Banner/Banner';
 import Carbodystyles from '../../Carbodystyles/Carbodystyles';
 import Features from '../../Features/Features';
-import Carcard from '../../Cards/Carcard';
+import CarServiceCard from '../../CarServiceCard/CarServiceCard';
+import cardData from '../../../Utility/carServiceCards/carServiceCards';
 const Home = () => {
   return (
-    <div>
+    <div className='home-container'>
       <CartypeProvider>
         <Banner />
         <Carbodystyles />
         <Features/>
-        <Carcard />
+        <div className='car-service-container'>
+          <div className="car-service-container__row">
+            {
+              cardData.map(({backGroundColor, icon, title, description, variant, buttonText}, index) => (
+                <CarServiceCard 
+                backGroundColor={backGroundColor}
+                icon={icon}
+                title={title}
+                description={description}
+                variant={variant}
+                buttonText={buttonText}
+                />
+              ))
+            }
+          </div>
+        </div>
       </CartypeProvider>
     </div>
   )
