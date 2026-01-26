89438a2032dc2ee99cf831f90cf973dda147a89b
Anjaliraghu123 <anjalifromkpm@gmail.com>
2026-01-26 13:03:04 +0530

    code re-factor

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -5,24 +5,26 @@ import Carbodystyles from '../../Carbodystyles/Carbodystyles';
 import Features from '../../Features/Features';
 import CarServiceCard from '../../CarServiceCard/CarServiceCard';
 import cardData from '../../../Utility/carServiceCards/carServiceCards';
+import "./home.scss";
+
 const Home = () => {
   return (
     <div className='home-container'>
       <CartypeProvider>
         <Banner />
         <Carbodystyles />
-        <Features/>
+        <Features />
         <div className='car-service-container'>
           <div className="car-service-container__row">
             {
-              cardData.map(({backGroundColor, icon, title, description, variant, buttonText}, index) => (
-                <CarServiceCard 
-                backGroundColor={backGroundColor}
-                icon={icon}
-                title={title}
-                description={description}
-                variant={variant}
-                buttonText={buttonText}
+              cardData.map(({ backGroundColor, icon, title, description, variant, buttonText }) => (
+                <CarServiceCard
+                  backGroundColor={backGroundColor}
+                  icon={icon}
+                  title={title}
+                  description={description}
+                  variant={variant}
+                  buttonText={buttonText}
                 />
               ))
             }
