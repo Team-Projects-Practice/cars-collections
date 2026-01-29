742d3c0735ef17a83c98ca88d5dc915053249c9e
Anjaliraghu123 <anjalifromkpm@gmail.com>
2026-01-29 19:37:11 +0530

    commit

--- src/Res/icons.js
+++ src/Res/icons.js
@@ -301,9 +301,10 @@ const SliderRightArrow = ({ color = "currentcolor", ...props }) => {
 </svg>
     )
 }
-
- export {
+ 
+export {
     MobileIcon, Downarrow, XIcon, UserIcon,MenuIcon,RightArrow,LeftArrow,SocialIcons,DotIcon,PricingTagIcon,
     DiamondIcon,FinanceIcon,CarIcon,ElectriccarIcon,Electriccar2Icon,CrosarowIcon,MilesIcon,CvtIcon,HybridIcon,
     ArrowIcon,BoockmarkIcon,SliderLeftArrow,SliderRightArrow,BrandArrowIcon
-     }
+     
+     } 
